b5f2e2790d3085b277ffb635b5051d6daa61c7e1
Nicola Giancecchi <nicola@wire.com>
2019-02-28 01:15:56 +0100

    Added CSV files and added TFJS example

--- tensorflow/ts.js
+++ tensorflow/ts.js
@@ -1,32 +1,84 @@
-// 6 ingressi reali
-// 3 uscite reali
-// 2 strati nascosti completamente connessi
-
-
 /*
+    // 6 ingressi reali 
+    I       // 2 strati nascosti completamente connessi        
+    I   H   H        
+    I   H   H   O   
+    I   H   H   O   // 3 uscite reali
+    I   H   H   O
     I   
-    I   H   H
-    I   H   H   O
-    I   H   H   O
-    I   H   H   O
-    I   
-
 */
-
 const model = tf.sequential();
 
-model.add(
-  tf.layers.dense({inputShape:[6], units:3, activation: "softmax"}),
-  tf.layers.dense({units:4}),
-  tf.layers.dense({units:4}),
-  tf.layers.dense({units:3})
-);
+//config for layer
+const config_hidden_1 = {
+  inputShape: [6],
+  activation: 'sigmoid',
+  units: 4
+}
+const config_hidden_2 = {
+  units: 4,
+  activation: 'sigmoid'
+}
 
-model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+const config_output = {
+  units: 3,
+  activation: 'sigmoid'
+}
 
-const xs = tf.tensor2d([[1], [2], [3], [4], [5], [6]], [6, 1]);
-const ys = tf.tensor2d([[1], [3], [5], [7], [9], [11]], [6, 1]);
+//defining the hidden and output layer
+const hidden1 = tf.layers.dense(config_hidden_1);
+const hidden2 = tf.layers.dense(config_hidden_2);
+const output = tf.layers.dense(config_output);
 
-model.fit(xs, ys, {epochs: 1000});
+//adding layers to model
+model.add(hidden1);
+model.add(hidden2);
+model.add(output);
 
-model.predict(tf.tensor2d([5], [1,1])).print();
+//define an optimizer
+const optimize = tf.train.sgd(0.1);
+
+//config for model
+const config = {
+  optimizer: optimize,
+  loss: 'meanSquaredError'
+}
+
+//compiling the model
+model.compile(config);
+console.log('Model Successfully Compiled');
+
+//Dummy training data
+const x_train = tf.tensor([
+  [0.1, 0.5, 0.1, 0.1, 0.5, 0.1],
+  [0.9, 0.3, 0.4, 0.1, 0.5, 0.1],
+  [0.4, 0.5, 0.5, 0.1, 0.5, 0.1],
+  [0.7, 0.1, 0.9, 0.1, 0.5, 0.1]
+])
+
+//Dummy training labels
+const y_train = tf.tensor([
+  [0.2, 0.8, 0.1],
+  [0.9, 0.10, 0.1],
+  [0.4, 0.6, 0.1],
+  [0.5, 0.5, 0.1]
+])
+
+//Dummy testing data
+const x_test = tf.tensor([
+  [0.9, 0.1, 0.5, 0.1, 0.5, 0.1]
+])
+
+train_data().then(function () {
+  console.log('Training is Complete');
+  console.log('Predictions :');
+  model.predict(x_test).print();
+})
+async function train_data() {
+  for (let i = 0; i < 10; i++) {
+    const res = await model.fit(x_train, y_train, epoch = 1000, batch_size = 10);
+    console.log(res.history.loss[0]);
+  }
+}
+
+// Original code https://medium.freecodecamp.org/get-to-know-tensorflow-js-in-7-minutes-afcd0dfd3d2f
